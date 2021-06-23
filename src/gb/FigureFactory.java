ebcb796eb1ccdbeac13851e1d21e3d473ea0b5dd
Kirill <benberov09@mail.ru>
2021-06-23 21:50:12 +0300

    HW3 - реализован паттерн "Абстрактная фабрика" с помощью класов Ball,Rectangle,Sprite, FigureFactory, BallFactory, RectangleFactory

---
+++ src/gb/FigureFactory.java
@@ -0,0 +1,5 @@
+package gb;
+
+public interface FigureFactory {
+    public  Sprite create();
+}
--- src/gb/Rectangle.java
+++ src/gb/Rectangle.java
@@ -2,8 +2,7 @@ package gb;
 
 import java.awt.*;
 
-public class Ball extends Sprite {
-
+public class Rectangle extends Sprite{
     private final Color color = new Color (
             (int)(Math.random() * 255),
             (int)(Math.random() * 255),
@@ -12,8 +11,7 @@ public class Ball extends Sprite {
     private float vX = (float)(100f + (Math.random() * 200f));
     private float vY = (float)(100f + (Math.random() * 200f));
 
-
-    Ball() {
+    Rectangle() {
         halfHeight = 20 + (float)(Math.random() * 50f);
         halfWidth = halfHeight;
     }
@@ -44,7 +42,7 @@ public class Ball extends Sprite {
     @Override
     void render(GameCanvas canvas, Graphics g) {
         g.setColor(color);
-        g.fillOval((int) getLeft(), (int) getTop(),
+        g.fillRect((int) getLeft(), (int) getTop(),
                 (int) getWidth(), (int) getHeight());
     }
 
